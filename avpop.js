755ef4dde85437a0ee4e2493eb5481590bc8f0b7
jkiggins <jkiggins794@gmail.com>
2018-05-19 22:55:49 -0400

    Transfer Workspace to Desktop

--- avpop.js
+++ avpop.js
@@ -3,23 +3,31 @@ var http = require('http');
 var api_key = "5G5MBKMY2EB14QDJ";
 var base_url = "https://www.alphavantage.co/query";
 
+
+// Eventually I want to load this list from a JSON file, and be able to add entries from a web interface
 var stocks = [
-    {name: 'APPL'}
-    ,{name: 'GOOGL'}
-    ,{name: 'TSLA'}
+    {
+        symbol: 'AAPL'
+    }
+    ,{
+        name: 'GOOGL'
+    }
+    ,{
+        name: 'TSLA'
+    }
 ];
 
-for (var stock in stocks){
+for (var i in stocks){
     var url = buildAlphaUrl(base_url, {
         "apikey": api_key
         ,"function": "TIME_SERIES_WEEKLY"
-        ,"symbol": stock.name
+        ,"symbol": stocks[i].symbol
     });
 
     console.log(url);
 }
 
-function buildAlphaUrl(options, base_url)
+function buildAlphaUrl(base_url, options)
 {
     if(!options.hasOwnProperty("apikey") || !options.hasOwnProperty("function")){
         throw "apikey and function are required";
@@ -30,9 +38,11 @@ function buildAlphaUrl(options, base_url)
     for(key in options)
     {
         if(options.hasOwnProperty(key)){
-            base_url += key + "=" + options[key];
+            base_url += key + "=" + options[key] + "&";
         }
     }
+
+    base_url = base_url.substr(0, base_url.length - 1);
 
     return base_url;
 }
